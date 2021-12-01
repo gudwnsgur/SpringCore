09b814c08e34eb2a6b5d15b79fa038f1948565f8
형준혁 <gudwnsgur12@naver.com>
2021-12-01 12:05:02 +0900

    [ADD] Spring Core - Member 도메인 개발

---
+++ src/main/java/study/spring/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package study.spring.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // 구현체가 없으면 npe(null pointer exception)이 뜨기 때문에 구현객체를 선택해야 한다.
+    // 다형성 이용
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return null;
+    }
+}
