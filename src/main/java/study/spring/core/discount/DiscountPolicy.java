e08f7e040ca806bb15e377bfd676697342188947
형준혁 <gudwnsgur12@naver.com>
2021-12-03 14:14:14 +0900

    [ADD] Spring Core - 주문 & 할인 도메인 개발

---
+++ src/main/java/study/spring/core/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package study.spring.core.discount;
+
+import study.spring.core.member.Member;
+
+public interface DiscountPolicy {
+    /**
+     *
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
